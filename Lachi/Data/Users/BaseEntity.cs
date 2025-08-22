75db6a167cc9f46d08c8f3a41a54446241c62aeb
PISHK3SVAT <mjek106206@gmail.com>
2025-08-22 16:04:13 +0330

    BaseEntity Added

---
+++ Lachi/Data/Users/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Lachi.Data.Users
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreateAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public int CreatedBy { get; set; }
+        public int UpdatedBy { get; set; }
+        public bool IsActive { get; set; }
+        public bool IsRemoved { get; set; }
+        public int RemovedBy { get; set; }
+    }
+}
