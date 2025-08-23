bf92045dfc93793af0d610f466f6b60f3ab67ab7
Lachi <amir.lachinani81@gmail.com>
2025-08-23 18:00:46 +0330

    Init BaseEntityConfigs Added

---
+++ Lachi/Data/Contexts/Configs/BaseEntityConfigs.cs
@@ -0,0 +1,41 @@
+﻿using Lachi.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Lachi.Data.Contexts.Configs
+{
+    public class BaseEntityConfigs : IEntityTypeConfiguration<IBaseEntity>
+    {
+        public void Configure(EntityTypeBuilder<IBaseEntity> builder)
+        {
+            builder.HasOne(b => b.CreatedBy)
+                .WithOne(u => u)
+                .HasForeignKey<IBaseEntity>(b => b.CreatedById);
+
+            builder.HasOne(b => b.UpdatedBy)
+                .WithOne(u => u)
+                .HasForeignKey<IBaseEntity>(b => b.UpdatedById);
+
+            builder.HasOne(b => b.RemovedBy)
+                .WithOne(u => u)
+                .HasForeignKey<IBaseEntity>(b => b.RemovedById);
+        }
+
+        public static void BaseConfigs<TEntity>(EntityTypeBuilder<IBaseEntity> builder) where TEntity : IBaseEntity
+        {
+            builder.HasOne(b => b.CreatedBy)
+                .WithOne(u => u)
+                .HasForeignKey<IBaseEntity>(b => b.CreatedById);
+
+            builder.HasOne(b => b.UpdatedBy)
+                .WithOne(u => u)
+                .HasForeignKey<IBaseEntity>(b => b.UpdatedById);
+
+            builder.HasOne(b => b.RemovedBy)
+                .WithOne(u => u)
+                .HasForeignKey<IBaseEntity>(b => b.RemovedById);
+        }
+    }
+
+
+}
